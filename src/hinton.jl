32d8cec325aa30695220a5286048e41e46f8154a
Abhinav Tushar <abhinav.tushar.vs@gmail.com>
2014-12-16 17:56:47 +0530

    Adds maximum weight constraint

--- src/hinton.jl
+++ src/hinton.jl
@@ -8,8 +8,14 @@ using Cairo
 
 function hintondiag(matrix::Matrix{Float64}; w_frame = 400, h_frame = 400)
     # Makes hinton diagram of the given matrix
+    #
+    # Parameters
+    # ----------
+    # matrix : the weight matrix to be visualized
+    # w_frame : width of the window in pixels
+    # h_frame : height of the window in pixels
 
-    win = Toplevel("hinton diagram", w_frame, h_frame);
+    win = Toplevel("weight matrix", w_frame, h_frame);
     can = Canvas(win);
     pack(can, expand = false, fill = "both");
 
@@ -21,18 +27,20 @@ function hintondiag(matrix::Matrix{Float64}; w_frame = 400, h_frame = 400)
 
     height, width = size(matrix);
 
-    w_ratio = w_frame / width;
-    h_ratio = h_frame / height;
+    w_cell = w_frame / width;
+    h_cell = h_frame / height;
 
-    cell_area = w_ratio * h_ratio;
+    cell_area = w_cell * h_cell * 0.7; # 0.7 to stop merging of cells 
+    max_weight = 2 ^ (ceil(log(maximum(abs(matrix))) / log(2)));
+    area_multiplier = cell_area / max_weight;
 
     for x in 1:width
         for y in 1:height
             weight = matrix[y, x]
             if weight > 0
-                draw_block(context, w_ratio * (x - 0.5), h_ratio * (y - 0.5), weight * cell_area, 1);
+                draw_block(context, w_cell * (x - 0.5), h_cell * (y - 0.5), min(1, weight) * area_multiplier, 1);
             elseif weight < 0
-                draw_block(context, w_ratio * (x - 0.5), h_ratio * (y - 0.5), - weight * cell_area, 0);
+                draw_block(context, w_cell * (x - 0.5), h_cell * (y - 0.5), min(1, -weight) * area_multiplier, 0);
             end
         end
     end
